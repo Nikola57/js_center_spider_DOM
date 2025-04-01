98b1ac922941110d7d28a527739275b8527ad3aa
Nikola57 <nikola.sribniy@gmail.com>
2025-04-01 17:20:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+// Отримуємо елементи
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+if (wall && spider) {
+  // Отримуємо розміри контейнера (стіни) та павука
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  // Обчислюємо позицію для центрування павука
+  const centerX = (wallRect.width - spiderRect.width - 10) / 2;
+  const centerY = (wallRect.height - spiderRect.height - 10) / 2;
+
+  // Встановлюємо позицію павука
+  spider.style.position = 'absolute';
+  spider.style.left = `${centerX}px`;
+  spider.style.top = `${centerY}px`;
+}
